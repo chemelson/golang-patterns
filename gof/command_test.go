08ed9738dc03d0fc9ad36d55e5bc3528e054b0cb
chemelson <ross03@mail.ru>
2024-06-06 19:09:25 +0300

    Implement Command pattern

---
+++ gof/command_test.go
@@ -0,0 +1,79 @@
+package gof
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCommand(t *testing.T) {
+	tv := &Tv{}
+
+	onCommand := &OnCommand{
+		device: tv,
+	}
+
+	offCommand := &OffCommand{
+		device: tv,
+	}
+
+	onButton := &Button{
+		command: onCommand,
+	}
+	onButton.press()
+
+	offButton := &Button{
+		command: offCommand,
+	}
+	offButton.press()
+}
+
+// Command interface
+type Command interface {
+	execute()
+}
+
+// Invoker
+type Button struct {
+	command Command
+}
+
+func (b *Button) press() {
+	b.command.execute()
+}
+
+// Receiver
+type Device interface {
+	on()
+	off()
+}
+
+type Tv struct {
+	isRunning bool
+}
+
+func (t *Tv) on() {
+	t.isRunning = true
+	fmt.Println("Turning tv on")
+}
+
+func (t *Tv) off() {
+	t.isRunning = false
+	fmt.Println("Turning tv off")
+}
+
+// Concrete commands
+type OnCommand struct {
+	device Device
+}
+
+func (c *OnCommand) execute() {
+	c.device.on()
+}
+
+type OffCommand struct {
+	device Device
+}
+
+func (c *OffCommand) execute() {
+	c.device.off()
+}
